7fb6255ad9dec42b3caffdf09a1a6f9207416319
Moo-CBT <stephenm@crossbrowsertesting.com>
2017-09-05 14:18:20 -0500

    Updated this in general

--- parallel/multithreaded/parallel.py
+++ parallel/multithreaded/parallel.py
@@ -13,20 +13,20 @@ def get_browser(caps):
         )
 
 browsers = [
-  {"os_api_name": "Win7x64-C2", "browser_api_name": "IE10", "name": "Python Parallel"},
-  {"os_api_name": "Win8.1", "browser_api_name": "Chrome43x64", "name": "Python Parallel"},
+  {"platform": "Windows 7 64-bit", "browserName": "Internet Explorer","version": "10", "name": "Python Parallel"},
+  {"platform": "Windows 8.1", "browserName": "Chrome", "version": "50", "name": "Python Parallel"},
 ]
 browsers_waiting = []
 
 
 def get_browser_and_wait(browser_data):
-    print ("starting %s\n" % browser_data["browser_api_name"])
+    print ("starting %s\n" % browser_data["browserName"])
     browser = get_browser(browser_data)
     browser.get("http://crossbrowsertesting.com")
     browsers_waiting.append({"data": browser_data, "driver": browser})
-    print ("%s ready" % browser_data["browser_api_name"])
+    print ("%s ready" % browser_data["browserName"])
     while len(browsers_waiting) < len(browsers):
-        print ("working on %s.... please wait" % browser_data["browser_api_name"])
+        print ("working on %s.... please wait" % browser_data["browserName"])
         browser.get("http://crossbrowsertesting.com")
         time.sleep(3)
 
